9ab2a61bbb30004c0dd71b94fddbc4fd460b256a
Esper77 <112155793+Esper77@users.noreply.github.com>
2022-11-19 16:10:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,57 @@ result_file.setsampwidth(FORMAT)
 result_file.setframerate(RATE)
 
 
-values = list(struct.unpack(f"<{frames_count*2}h", frames))
+def slow_sound(values, channels):
+    distorted = []
+    for i in range(1, len(values)):
+        distorted.append(values[i-1])
+        if channels == 2:
+            distorted.append([(values[i-1][0] + values[i][0]) // 2, (values[i-1][1] + values[i][1]) // 2])
+        else:
+            distorted.append([(values[i-1][0] + values[i][0]) // 2])
+    distorted.append(values[len(values)-1])
+    return distorted
 
-distorted = []
 
-for i in range(len(values)):
-    if i % 2 == 0:
-        distorted.append(values[i])
+def speedup_sound(values):
+    distorted = []
+    for i in range(len(values)):
+        if i % 2 == 0:
+            distorted.append(values[i])
+    return distorted
 
-frames = struct.pack(f"<{frames_count}h", *distorted)
+
+def reverse_sound(values):
+    return list(values.__reversed__())
+
+
+def pairs_unpack(in_list):
+    out_list = []
+    for x in in_list:
+        out_list += x
+    return out_list
+
+
+def pairs_pack(values, channels):
+    out_list = []
+    if channels == 2:
+        for i in range(0, len(values), 2):
+            out_list.append([values[i], values[i+1]])
+    else:
+        for i in range(len(values)):
+            out_list.append([values[i]])
+    return out_list
+
+
+values = list(struct.unpack(f"<{frames_count*CHANNELS}h", frames))
+
+
+paired_values = pairs_pack(values, CHANNELS)
+
+
+out_values = pairs_unpack(speedup_sound(slow_sound(paired_values, CHANNELS)))
+
+print(out_values)
+
+frames = struct.pack(f"<{len(out_values)}h", *out_values)
 result_file.writeframes(frames)
